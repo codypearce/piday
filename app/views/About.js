eaeffb42e1729c633f88fba274b58258839d8606
Cody Pearce <codyapearce@gmail.com>
2017-02-14 18:40:43 -0700

    moved learn and about back button to top left

--- app/views/About.js
+++ app/views/About.js
@@ -26,26 +26,35 @@ export default class About extends React.Component {
             <View style={{
                 backgroundColor: '#1976D2',
                 flex: 1,
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center'
-            }}
+            }}>
+                <View  style={{top: 5, left: 5}}>
+                    <TouchableHighlight onPress={ () => this._navigate('Home') }>
+                        <Text style={{color: 'white'}}>Back</Text>
+                    </TouchableHighlight>
+                </View>
+                <View style={{
+                    flex: 1,
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}
 
-            >
-                <Text
-                    style={{
-                        color: 'white',
-                        fontSize: 24
-                    }}
                 >
-                    About
-                </Text>
-                <TouchableHighlight  onPress={ () => this._navigate('Home') }>
-                    <Text>
-                        Home
-                    </Text>
-                </TouchableHighlight>
+                    <View
+                        style={{
+                            marginTop: -100,
+                            marginBottom: 50
+                        }}>
+                        <Text
+                            style={{
+                                color: 'white',
+                                fontSize: 24
+                            }}>
+                            About
+                        </Text>
+                    </View>
 
+                </View>
             </View>
         )
     }
