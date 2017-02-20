c074607428a1355d6d5e0c01d733de222260fb97
Cody Pearce <codyapearce@gmail.com>
2017-02-19 18:14:19 -0700

    add a few questions, question component, start quiz

--- app/views/Quiz.js
+++ app/views/Quiz.js
@@ -12,13 +12,36 @@ import {
 export default class Quiz extends React.Component {
     constructor(props) {
         super(props);
+        this.startQuiz = this.startQuiz.bind(this);
+        this.state = {
+            isStarted: false,
+            questionIdx: null,
+        }
     }
     _navigate(route){
       this.props.navigator.push({
         name: route,
       })
     }
+    randomQuestion() {
+        let rand = Math.round(Math.random() * 3);
+        this.setState({
+            questionIdx: rand
+        })
+    }
+    startQuiz() {
+        this.randomQuestion();
+        this.setState({
+            isStarted: true
+        })
+    }
     render() {
+        let content = null;
+        if(this.state.isStarted == false) {
+            content = <Button onPress={this.startQuiz} title="Start Quiz" />
+        } else if(this.state.isStarted == true) {
+            content = <Question questionIdx={this.state.questionIdx} />
+        }
         return (
             <View style={{
                 backgroundColor: '#1976D2',
@@ -42,15 +65,62 @@ export default class Quiz extends React.Component {
                             marginTop: -100,
                             marginBottom: 50
                         }}>
-                        <Text
-                            style={{
-                                color: 'white',
-                                fontSize: 24
-                            }}>
-                            Quiz
-                        </Text>
+                        {content}
                     </View>
 
+                </View>
+            </View>
+        )
+    }
+}
+const questions1 = [
+    ["What day is Pi day", ["3/14", "14/3", "22/7", "Everyday is pi day"], "3/14"],
+    ["When did Pi day start", ["1978", "1565", "1988", "2007"], "1988"],
+    ["When did congress pass a law recognizing Pi Day as a national day?", ["2007", "2009", "1988", "1989"], "2009"],
+    ["What day is Pi Approximation day?", ["3/14", "4/14", "22/7", "15/1"], "22/7"],
+    ["How is Pi defined mathematically", ["Circumference / diamter", "2 r^2", "1 / r^2", "I'ts not"], "Circumference / diamter"]
+]
+const questions = [
+    {
+        question: "What day is Pi day",
+        choices:  ["3/14", "14/3", "22/7", "Everyday is pi day"],
+        answer: '3/14'
+    },
+    {
+        question: "When did Pi day start",
+        choices:  ["1978", "1565", "1988", "2007"],
+        answer: '1988'
+    },
+    {
+        question: "When did congress pass a law recognizing Pi Day as a national day?",
+        choices:  ["2007", "2009", "1988", "1989"],
+        answer: '2009'
+    },
+
+]
+class Question extends React.Component {
+    constructor(props) {
+        super(props);
+        actualQuestion = questions[this.props.questionIdx]
+        this.state = {
+            question: actualQuestion.question,
+            choice1: actualQuestion.choices[0],
+            choice2: actualQuestion.choices[1],
+            choice3: actualQuestion.choices[2],
+            choice4: actualQuestion.choices[3],
+            answer: actualQuestion.answer
+        }
+    }
+    render() {
+
+        return(
+            <View>
+                <Text> {this.state.question}</Text>
+                <View>
+                    <Text> {this.state.choice1}</Text>
+                    <Text> {this.state.choice2}</Text>
+                    <Text> {this.state.choice3}</Text>
+                    <Text> {this.state.choice4}</Text>
                 </View>
             </View>
         )
