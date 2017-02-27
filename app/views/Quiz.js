c22a15676b08191959915afeca02e55494dbb5f3
Cody Pearce <codyapearce@gmail.com>
2017-02-26 19:02:04 -0700

    questions are now in own component and imported into quiz component

--- app/views/Quiz.js
+++ app/views/Quiz.js
@@ -9,6 +9,8 @@ import {
   TouchableHighlight
 } from 'react-native';
 
+import questionsSrc from '../components/Questions';
+
 export default class Quiz extends React.Component {
     constructor(props) {
         super(props);
@@ -231,101 +233,3 @@ const styles = StyleSheet.create({
       width: 250
   }
 });
-const questionsSrc = [
-   {
-       question: "When is Pi Day celebrated?",
-       choices:  ["March 14", "June 28", "October 14", "December 25"],
-       answer: 'March 14'
-   },
-   {
-       question: "In what year did the U.S. House of Representatives first officially recognize Pi Day?",
-       choices:  ["1988", "1995", "2009", "2013"],
-       answer: '2009'
-   },
-   {
-       question: "The birthday of which famous scientist falls on Pi Day?",
-       choices:  ["Leonhard Euler", "Albert Einstein", "Isaac Newton", "Archimedes"],
-       answer: 'Albert Einstein '
-   },
-   {
-       question: "Where were the first known Pi Day celebrations held?",
-       choices:  ["The San Francisco Exploratorium", "Caltech", "The Liberty Science Center", "M.I.T."],
-       answer: 'The San Francisco Exploratorium'
-   },
-   {
-       question: "Who organized the first known Pi Day celebrations?",
-       choices:  ["Larry Shaw", "Larry Page", "Larry Wilmore", "Larry Johnson"],
-       answer: 'Larry Shaw'
-   },
-   {
-       question: "When is Pi Approximation Day celebrated?",
-       choices:  ["July 22", "March 14", "July 14", "March 22"],
-       answer: 'July 22'
-   },
-   {
-       question: "What is the circumference of a circle?",
-       choices:  ["The distance across the widest part of the circle", "The linear distance around the circle", "The distance from the center of the circle to its edge", "The area of a square inscribed in the circle"],
-       answer: 'The linear distance around the circle'
-   },
-   {
-       question: "What is the diameter of a circle?",
-       choices:  ["The distance across the widest part of the circle", "The linear distance around the circle", "The distance from the center of the circle to its edge", "The area of a square inscribed in the circle"],
-       answer: 'The distance across the widest part of the circle'
-   },
-   {
-       question: "Which of the following has not yet been proven?",
-       choices:  ["Pi is an irrational number", "Pi is a mathematical constant", "Pi is a normal number", "Pi is a transcendental number"],
-       answer: 'Pi is a normal number'
-   },
-   {
-       question: "In what year was pi proven to be transcendental?",
-       choices:  ["1751", "1882", "1917", "1949"],
-       answer: '1882'
-   },
-   {
-       question: "Which of the following is a rational number?",
-       choices:  ["The ratio of an octagon's perimeter to its width", "The ratio of a square's perimeter to its width", "The ratio of a circle's circumference to its diameter", "The golden ratio"],
-       answer: 'The ratio of a square\'s perimeter to its width'
-   },
-   {
-       question: "Which of the following would give the best approximation for pi?",
-       choices:  ["The ratio of a square's perimeter to its width", "The ratio of a pentagon's perimeter to its width", "The ratio of a hexagon's perimeter to its width", "The ratio of an octagon's perimeter to its width"],
-       answer: 'The ratio of an octagon\'s perimeter to its width'
-   },
-   {
-       question: "Pi is believed to be a normal number. Which of the following is true of normal numbers?",
-       choices:  ["The digits of a normal number have a set pattern", "A normal number can be expressed in terms of a geometric figure", "No digit in a normal number occurs more frequently than any other digit", "A number that is absolutely normal might be normal in binary but not in base 10"],
-       answer: 'No digit in a normal number occurs more frequently than any other digit'
-   },
-   {
-       question: "Which approximation for pi is closest to that found in the Rhind Papyrus?",
-       choices:  ["Pi = 3", "Pi = 3 1/7", "Pi = 3.1605", "Pi = 3.14"],
-       answer: 'Pi = 3.1605'
-   },
-   {
-       question: "Which approximation for pi is found in the Old Testament of the Bible?",
-       choices:  ["Pi = 3", "Pi = 3 1/7", "Pi = 3.1605", "Pi = 3.14"],
-       answer: 'Pi = 3'
-   },
-   {
-       question: "Which mathematician used polygons with 24,576 sides to approximate pi?",
-       choices:  ["Archimedes of Syracuse", "Zu Chongzhi", "François Viète", "Bryson of Heraclea "],
-       answer: 'Zu Chongzhi'
-   },
-   {
-       question: "Who is credited with popularizing the use of the greek letter pi to represent the ratio of the circumference of a circle to its diameter?",
-       choices:  ["Leonhard Euler", "Ferdinand von Lindemann", "Johann Heinrich Lambert", "François Viète"],
-       answer: 'Leonhard Euler'
-   },
-   {
-       question: "As of 2016, what is the approximate record for digits of pi calculated?",
-       choices:  ["About 130 million", "About 58 billion", "About 22 trillion", "About 382 trillion"],
-       answer: 'About 22 trillion'
-   },
-   {
-       question: "Which computer calculated 2,037 digits of pi in 1949?",
-       choices:  ["Atanasoff–Berry Computer (ABC)", "Electronic Numerical Integrator and Calculator (ENIAC)", "Deep Blue", "Babbage Difference Engine"],
-       answer: 'Electronic Numerical Integrator and Calculator (ENIAC)'
-   },
-
-]
