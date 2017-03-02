a11f0f600a1c9c1e47f137c9069ecccd8b9b0044
Cody Pearce <codyapearce@gmail.com>
2017-03-02 07:30:37 -0700

    top section, title, settings gear

--- app/views/Home.js
+++ app/views/Home.js
@@ -4,7 +4,8 @@ import {
   Text,
   View,
   Navigator,
-  TouchableHighlight
+  TouchableHighlight,
+  Image
 } from 'react-native';
 
 export default class Home extends React.Component {
@@ -18,61 +19,85 @@ export default class Home extends React.Component {
     }
     render() {
         return (
-            <View style={styles.container}>
-                <Text style={styles.title}>
-                    Pi day
-                </Text>
-                <TouchableHighlight  onPress={ () => this._navigate('Game') }>
-                    <Text style={styles.centerNav}>
-                        Play
-                    </Text>
-                </TouchableHighlight>
-                <TouchableHighlight  onPress={ () => this._navigate('Timed') }>
-                    <Text style={styles.centerNav}>
-                        Timed
-                    </Text>
-                </TouchableHighlight>
-                <TouchableHighlight  onPress={ () => this._navigate('Quiz') }>
-                    <Text style={styles.centerNav}>
-                        Quiz
-                    </Text>
-                </TouchableHighlight>
-                <TouchableHighlight  onPress={ () => this._navigate('Learn') }>
-                    <Text style={styles.centerNav}>
-                        Learn
-                    </Text>
-                </TouchableHighlight>
-                <TouchableHighlight  onPress={ () => this._navigate('Settings') }>
-                    <Text style={styles.centerNav}>
-                        Settings
-                    </Text>
-                </TouchableHighlight>
-                <TouchableHighlight  onPress={ () => this._navigate('Records') }>
-                    <Text style={styles.centerNav}>
-                        Records
-                    </Text>
-                </TouchableHighlight>
+            <View style={styles.wrapper}>
+                <View style={styles.top}>
+                    <Image
+                        style={styles.settings}
+                        source={require('../images/settings.png')}
+                    />
+                    <Text style={styles.title}>PI DAY</Text>
+                </View>
+                <View style={styles.container}>
+                    <TouchableHighlight  onPress={ () => this._navigate('Game') }>
+                        <Text style={styles.centerNav}>
+                            Play
+                        </Text>
+                    </TouchableHighlight>
+                    <TouchableHighlight  onPress={ () => this._navigate('Timed') }>
+                        <Text style={styles.centerNav}>
+                            Timed
+                        </Text>
+                    </TouchableHighlight>
+                    <TouchableHighlight  onPress={ () => this._navigate('Quiz') }>
+                        <Text style={styles.centerNav}>
+                            Quiz
+                        </Text>
+                    </TouchableHighlight>
+                    <TouchableHighlight  onPress={ () => this._navigate('Learn') }>
+                        <Text style={styles.centerNav}>
+                            Learn
+                        </Text>
+                    </TouchableHighlight>
+                    <TouchableHighlight  onPress={ () => this._navigate('Settings') }>
+                        <Text style={styles.centerNav}>
+                            Settings
+                        </Text>
+                    </TouchableHighlight>
+                    <TouchableHighlight  onPress={ () => this._navigate('Records') }>
+                        <Text style={styles.centerNav}>
+                            Records
+                        </Text>
+                    </TouchableHighlight>
+                </View>
             </View>
         )
     }
 }
 const styles = StyleSheet.create({
-    container: {
+    wrapper: {
         backgroundColor: '#1976D2',
         flex: 1,
+    },
+    container: {
+
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    centerNav: {
+        color: 'white',
+        fontSize: 18,
+        marginBottom: 5
+    },
+    top: {
+        backgroundColor: '#d7933f',
+        height: 200,
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center'
     },
     title: {
-        color: 'white',
-        fontSize: 24,
-        marginTop: -100,
-        marginBottom: 100
+        color: '#810000',
+        fontSize: 58,
+        fontWeight: "700",
+        fontFamily: 'Roboto'
     },
-    centerNav: {
-        color: 'white',
-        fontSize: 18,
-        marginBottom: 5
+    settings: {
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        opacity: 0.5,
+        transform: [{scale: .7}]
     }
 });
