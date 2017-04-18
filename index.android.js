2e3d8cfba7907e83820dfd1ac3c406997f0a8cff
Cody Pearce <codyapearce@gmail.com>
2017-04-17 18:48:27 -0600

    fix index.android linting problems

--- index.android.js
+++ index.android.js
@@ -7,25 +7,22 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  StyleSheet,
-  Text,
-  View,
   Navigator,
   BackAndroid
 } from 'react-native';
 
 
-import Home from './app/views/Home'
-import Memorize from './app/views/Memorize'
-import Game from './app/views/Game'
-import Timed from './app/views/Timed'
-import Quiz from './app/views/Quiz'
-import Learn from './app/views/Learn'
-import Settings from './app/views/Settings'
-import Donate from './app/views/Donate'
-import About from './app/views/About'
-import Records from './app/views/Records'
-import LearnPage from './app/views/LearnPage'
+import Home from './app/views/Home';
+import Memorize from './app/views/Memorize';
+import Game from './app/views/Game';
+import Timed from './app/views/Timed';
+import Quiz from './app/views/Quiz';
+import Learn from './app/views/Learn';
+import Settings from './app/views/Settings';
+import Donate from './app/views/Donate';
+import About from './app/views/About';
+import Records from './app/views/Records';
+import LearnPage from './app/views/LearnPage';
 
 
 export default class piday extends Component {
@@ -41,68 +38,68 @@ export default class piday extends Component {
                return false;
         });
         if(route.name == 'Home') {
-            return <Home navigator={navigator} />
+            return <Home navigator={navigator} />;
         }
         if(route.name == 'Memorize') {
-            return <Memorize navigator={navigator} />
+            return <Memorize navigator={navigator} />;
         }
         if(route.name == 'Game') {
-            return <Game navigator={navigator} />
+            return <Game navigator={navigator} />;
         }
         if(route.name == 'Timed') {
-            return <Timed navigator={navigator} />
+            return <Timed navigator={navigator} />;
         }
         if(route.name == 'Quiz') {
-            return <Quiz navigator={navigator} />
+            return <Quiz navigator={navigator} />;
         }
         if(route.name == 'Learn') {
-            return <Learn navigator={navigator} />
+            return <Learn navigator={navigator} />;
         }
 
         // Learn Page Sections
         if(route.name == 'Whatispi') {
-            return <LearnPage navigator={navigator} title={'What is Pi?'} />
+            return <LearnPage navigator={navigator} title={'What is Pi?'} />;
         }
         if(route.name == 'Whatispiday') {
-            return <LearnPage navigator={navigator} title={'What is Pi day?'} />
+            return <LearnPage navigator={navigator} title={'What is Pi day?'} />;
         }
         if(route.name == 'Propertiesofpi') {
-            return <LearnPage navigator={navigator} title={'The Properties of Pi'} />
+            return <LearnPage navigator={navigator} title={'The Properties of Pi'} />;
         }
         if(route.name == 'Historyofpi') {
-            return <LearnPage navigator={navigator} title={'The History of Pi'}  />
+            return <LearnPage navigator={navigator} title={'The History of Pi'}  />;
         }
         if(route.name == 'Computingdigits') {
-            return <LearnPage navigator={navigator} title={'Computing Digits of Pi'} />
+            return <LearnPage navigator={navigator} title={'Computing Digits of Pi'} />;
         }
         if(route.name == 'Pimathsci') {
-            return <LearnPage navigator={navigator} title={'Pi in Math & Science'} />
+            return <LearnPage navigator={navigator} title={'Pi in Math & Science'} />;
         }
         if(route.name == 'Pipop') {
-            return <LearnPage navigator={navigator} title={'Pi in Popular Culture'}  />
+            return <LearnPage navigator={navigator} title={'Pi in Popular Culture'}  />;
         }
         if(route.name == 'Glossary') {
-            return <LearnPage navigator={navigator} title={'Glossary'} />
+            return <LearnPage navigator={navigator} title={'Glossary'} />;
         }
 
         if(route.name == 'Settings') {
-            return <Settings navigator={navigator} />
+            return <Settings navigator={navigator} />;
         }
         if(route.name == 'Records') {
-            return <Records navigator={navigator} />
+            return <Records navigator={navigator} />;
         }
         if(route.name == 'Donate') {
-            return <Donate navigator={navigator} />
+            return <Donate navigator={navigator} />;
         }
         if(route.name == 'About') {
-            return <About navigator={navigator} />
+            return <About navigator={navigator} />;
         }
 
     }
     render() {
         return (
             <Navigator
-                initialRoute={{title: "Home", name: 'Home', component: Home}}
+                initialRoute={{title: 'Home', name: 'Home', component: Home}}
                 renderScene={this.renderScene}
             />
         );
